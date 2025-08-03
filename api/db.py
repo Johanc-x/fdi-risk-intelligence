04ab28445323f56e6099b59f0ec6871e13d3d367
Jhojan Cachay <johancr.econ@gmail.com>
2025-08-03 18:30:46 +0200

    🔐 Update db.py to use environment variables for Oracle credentials

--- api/db.py
+++ api/db.py
@@ -1,10 +1,13 @@
 # api/db.py
 import cx_Oracle
+import os
 
 def conectar_oracle():
     try:
         dsn = cx_Oracle.makedsn("localhost", 1521, service_name="XE")
-        conn = cx_Oracle.connect("system", "Success_0425", dsn)
+        user = os.getenv("ORACLE_USER", "usuario_por_defecto")
+        password = os.getenv("ORACLE_PASS", "contraseña_por_defecto")
+        conn = cx_Oracle.connect(user, password, dsn)
         return conn
     except cx_Oracle.Error as e:
         print("❌ Error al conectar con Oracle:", e)
